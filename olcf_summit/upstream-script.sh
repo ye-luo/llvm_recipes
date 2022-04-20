5f037c4bc71db12bc6848ab82488bfaec3e01fd4
Ye Luo <yeluo@anl.gov>
2022-04-19 21:32:05 -0400

    Update Summit recipe.

--- olcf_summit/upstream-script.sh
+++ olcf_summit/upstream-script.sh
@@ -1,8 +1,13 @@
 prefix=/ccs/home/yeluo/opt/llvm-clang
-install_path=/ccs/proj/mat151/opt/llvm
+install_path=/gpfs/alpine/mat151/world-shared/opt/llvm
+#/ccs/proj/mat151/opt/llvm
 
-module load git cmake gcc/8.1.1 cuda
+module load git cmake gcc/9.3.0 cuda/11.0.3
+GCC_ROOT=$(dirname -- $(dirname -- `which gcc`))
 
+module list
+
+echo GCC_ROOT is $GCC_ROOT
 cd $prefix
 
 if [ ! -d $prefix/llvm-project ] ; then
@@ -17,19 +22,20 @@ if [ $# -eq 0 ] ; then
   build_folder=build_mirror_offload_main
   INSTALL_FOLDER=$install_path/main-`date +%Y%m%d`
   git co main
-#  git pull
+  git pull
 
   #git fetch jdoerfert
   #git co feature/declare_variant_begin
   #git reset --hard jdoerfert/feature/declare_variant_begin
-  PACKAGES="clang;compiler-rt"
+  PACKAGES="clang;compiler-rt;openmp"
+  RUNTIMES=""
 elif [ $1 == "patched" ] ; then
   echo building patch
   echo -----------------------------------
   build_folder=build_mirror_offload_patched
   INSTALL_FOLDER=$install_path/main-patched
-  #INSTALL_FOLDER=$install_path/main-20210112
-  PACKAGES="clang;compiler-rt"
+  PACKAGES="clang;compiler-rt;openmp"
+  RUNTIMES=""
 else
   echo building release $1
   echo -----------------------------------
@@ -37,15 +43,16 @@ else
   INSTALL_FOLDER=$install_path/release-$1
   git fetch
   git co llvmorg-$1
-  PACKAGES="clang;compiler-rt"
+  PACKAGES="clang;compiler-rt;openmp"
+  RUNTIMES="libcxxabi;libcxx"
 fi
 
-#rm -rf $prefix/$build_folder
+rm -rf $prefix/$build_folder
 mkdir $prefix/$build_folder ; cd $prefix/$build_folder
 
 cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ \
     -DCMAKE_BUILD_TYPE=Release \
-    -DGCC_INSTALL_PREFIX=/sw/summit/gcc/8.1.1 \
+    -DGCC_INSTALL_PREFIX=$GCC_ROOT \
     -DCMAKE_INSTALL_PREFIX=$INSTALL_FOLDER \
     -DLLVM_ENABLE_BACKTRACES=ON \
     -DLLVM_ENABLE_WERROR=OFF \
@@ -53,7 +60,7 @@ cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ \
     -DLLVM_ENABLE_RTTI=ON \
     -DLLVM_ENABLE_ASSERTIONS=ON \
     -DLLVM_ENABLE_PROJECTS="$PACKAGES" \
-    -DLLVM_ENABLE_RUNTIMES="libcxxabi;libcxx;openmp" \
+    -DLLVM_ENABLE_RUNTIMES="$RUNTIMES" \
     -DOPENMP_ENABLE_LIBOMPTARGET=ON \
     -DLIBOMPTARGET_NVPTX_COMPUTE_CAPABILITIES=70 \
     -DCLANG_OPENMP_NVPTX_DEFAULT_ARCH=sm_70 \
