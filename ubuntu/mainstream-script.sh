bf0f790c8627a2551f55af18f4af794b0cf7369d
Ye Luo <yeluo@anl.gov>
2025-02-03 15:02:07 -0600

    Update ubuntu recipe

--- ubuntu/mainstream-script.sh
+++ ubuntu/mainstream-script.sh
@@ -1,8 +1,13 @@
 prefix=`pwd`
 install_path=/soft/llvm
 
-CC=gcc
-CXX=g++
+export CMAKE_PREFIX_PATH=/soft/spack/opt/spack/linux-ubuntu20.04-zen2/gcc-9.4.0/libffi-3.4.6-s4cthjwrnauavtc24wk3u4ceuzu6b5gi:$CMAKE_PREFIX_PATH
+
+module load llvm/release-18.1
+module list
+
+CC=clang
+CXX=clang++
 
 cd $prefix
 
@@ -15,7 +20,7 @@ cd $prefix/llvm-project
 if [ $# -eq 0 ] ; then
   echo building main
   echo -----------------------------------
-  build_folder=build_mirror_offload_main
+  build_folder=build_llvm_main
   INSTALL_FOLDER=$install_path/main-`date +%Y%m%d`
   git co main
   git pull
@@ -23,20 +28,20 @@ if [ $# -eq 0 ] ; then
   #git fetch jdoerfert
   #git co feature/declare_variant_begin
   #git reset --hard jdoerfert/feature/declare_variant_begin
-  PACKAGES="clang;compiler-rt;lld;openmp"
-  RUNTIMES="libcxxabi;libcxx"
+  PACKAGES="clang;lld;mlir;flang"
+  RUNTIMES="openmp;offload;compiler-rt"
 elif [ $1 == "patched" ] ; then
   echo building patch
   echo -----------------------------------
-  build_folder=build_mirror_offload_patched
+  build_folder=build_llvm_patched
   INSTALL_FOLDER=$install_path/main-patched
   #INSTALL_FOLDER=$install_path/main-20210112
-  PACKAGES="clang;compiler-rt;lld;openmp"
-  RUNTIMES="libcxxabi;libcxx"
+  PACKAGES="clang;lld;mlir;flang"
+  RUNTIMES="openmp;offload;compiler-rt"
 else
   echo building release $1
   echo -----------------------------------
-  build_folder=build_mirror_offload_release
+  build_folder=build_llvm_release
   git fetch
   version=$1
   git co llvmorg-$version
@@ -50,10 +55,11 @@ else
     fi
   fi
   INSTALL_FOLDER=$install_path/release-$version
-  PACKAGES="clang;compiler-rt;lld;openmp"
-  RUNTIMES="libcxxabi;libcxx"
+  PACKAGES="clang;lld;mlir;flang"
+  RUNTIMES="openmp;offload;compiler-rt"
 fi
 
+echo install folder $INSTALL_FOLDER
 rm -rf $prefix/$build_folder
 mkdir $prefix/$build_folder ; cd $prefix/$build_folder
 
@@ -68,11 +74,11 @@ cmake -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX \
     -DLLVM_ENABLE_ASSERTIONS=ON \
     -DLLVM_ENABLE_PROJECTS="$PACKAGES" \
     -DLLVM_ENABLE_RUNTIMES="$RUNTIMES" \
-    -DCLANG_OPENMP_NVPTX_DEFAULT_ARCH=sm_80 \
     -DLIBOMPTARGET_ENABLE_DEBUG=ON \
-    ../llvm-project/llvm
+    -DLLVM_PARALLEL_LINK_JOBS=4 \
+    -DLLVM_BINUTILS_INCDIR=/usr/include \
+    ../llvm-project/llvm && make -j15 -k
 
-make -j15 && make -j15 && sudo make -j15 install
+make -j32 install && echo --gcc-install-dir=/usr/lib/gcc/x86_64-linux-gnu/11 > $INSTALL_FOLDER/bin/x86_64-unknown-linux-gnu.cfg && chmod -w -R $INSTALL_FOLDER
 
-#    -DLLVM_EXTERNAL_PROJECTS="device-libs" \
-#    -DLLVM_EXTERNAL_DEVICE_LIBS_SOURCE_DIR=../ROCm-Device-Libs \
+chmod -R -w $INSTALL_FOLDER
